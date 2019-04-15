101c7e055da0d67dd8550473246bef68b3341581
Ajimol Das <745042@student.tor.loc>
2019-04-15 16:45:43 -0400

    third  commit

--- mapscript.js
+++ mapscript.js
@@ -17,4 +17,8 @@ function initialize(){
     map3 = new google.maps.Map(document.getElementById('map_canvas3'), mapOptions3);
 
     }
-    google.maps.event.addDomListener(window,'load',initialize);
+    newFunction();
+function newFunction() {
+    google.maps.event.addDomListener(window, 'load', initialize);
+}
+
